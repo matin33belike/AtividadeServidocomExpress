3e322d76b74ddebe15dd2abd07104df18e5eee9f
Mateus Moreira <moreiradasilvamateus214@gmail.com>
2025-10-15 01:08:19 -0400

    mais alterações

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { Database } from "./databases.js"
 const app = express()
 app.use(express.json())
 
@@ -8,13 +9,18 @@ app.get ('/', (req, res)=> {
 
 const usuarios = []
 app.get ('/usuario', (req, res)=> {
-    res.status(200).json(usuarios)
+    const data = Database.select("usuario")
+    res.status(200).json(data)
 })
 
 app.post('/usuario', (req, res)=> {
-    const {nome, idade} = req.body
-    usuarios.push({nome, idade})
-    console.log(nome, idade)
-
-    res.status(201).send("Usuário Criado")
+    const {nome, idade, email} = req.body
+    if (nome, idade, email) {
+        Database.insert("usuario", {nome, idade, email})
+        return res.status(201).send("Usuário Criado")
+    }
+    return res.status(400).send("Informações inválidas")
+})
+app.listen(3333, () => {
+    console.log("Servidor rodando na porta 3333")
 })
