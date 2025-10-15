22c2c88659537681b0cc3eb451a6a9f542f780e5
Mateus Moreira <moreiradasilvamateus214@gmail.com>
2025-10-15 00:01:12 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 import express from "express"
 const app = express()
+app.use(express.json())
 
 app.get ('/', (req, res)=> {
     res.send("Você está na página inicial")
 })
 
-app.post('/', (req, res)=> {
-    res.send("Você esta na página inicial usando o método post")
+const usuarios = []
+app.get ('/usuario', (req, res)=> {
+    res.status(200).json(usuarios)
+})
+
+app.post('/usuario', (req, res)=> {
+    const {nome, idade} = req.body
+    usuarios.push({nome, idade})
+    console.log(nome, idade)
+
+    res.status(201).send("Usuário Criado")
 })
